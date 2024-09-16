6a502032b12588960f1f0b0919b7fa3a8f1e281a
Geovani <geovanifranca76@gmail.com>
2024-09-15 21:56:10 -0300

    index feito

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/StylesLandingPage.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css">
+    <title>Lanchonete França</title>
+</head>
+<body>
+    <main>
+        <section id="container1">
+           <div id="init-lading-page"> 
+                <p>Bem vindos a lanchonete França</p>
+                <br>    
+                <p>Aqui, seu lanche é preparado por você mesmo</p>
+
+                <div class="btnAcess">
+                    <a href="#"> Acessar </a>
+                </div>
+           </div>
+        </section>
+
+        <section>
+          <!-- background  -->
+        </section>     
+
+    </main>
+
+    <footer>
+
+    </footer>
+    
+</body>
+</html>
